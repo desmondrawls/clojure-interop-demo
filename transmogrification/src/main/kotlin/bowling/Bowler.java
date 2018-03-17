b776e84c18fea841dddd0fa1a640fa55084d3a8c
Desmond Rawls <captaingrover@gmail.com>
2018-03-17 14:53:06 -0700

    INTRODUCE A RIFF FUNCTION.

---
+++ transmogrification/src/main/kotlin/bowling/Bowler.java
@@ -0,0 +1,10 @@
+package bowling;
+
+import java.util.function.Function;
+
+public class Bowler implements Function<String, String> {
+
+	public String apply(String name) {
+		return "BOWLING: " + name;
+	}
+}

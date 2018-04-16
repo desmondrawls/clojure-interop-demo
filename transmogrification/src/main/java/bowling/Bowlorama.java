f583ece880bdf68e251c85dbee4f16a920c1813a
Desmond Rawls <captaingrover@gmail.com>
2018-04-15 21:24:35 -0700

    RIFF

---
+++ transmogrification/src/main/java/bowling/Bowlorama.java
@@ -0,0 +1,17 @@
+package bowling;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import scoring.shower;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class Bowlorama implements Function<String, Outcome<Integer, List<BowlingFailures>>> {
+
+    public Outcome<Integer, List<BowlingFailures>> apply(String body) {
+        DocumentContext documentContext = JsonPath.parse(body);
+        List<Integer> rolls = documentContext.read("$.rolls[*]");
+        return shower.scorer().score(rolls);
+    }
+}
